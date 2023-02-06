55258aa4870ae4dd0a24216c30eca486a47ed951
Lemon Rose <yash.kul69@gmail.com>
2023-02-06 16:39:06 +0530

    tests: add break tests

--- test/test_output.py
+++ test/test_output.py
@@ -3017,3 +3017,32 @@ def test_colalign_with_mixed_data():
     )
     result = tabulate(test_table, headers=table_headers, colalign=("left", "right"))
     assert_equal(expected, result)
+
+def test_break_long_words():
+    "Output: Default table output, with breakwords true."
+    table_headers = ["h1", "h2", "h3"]
+    test_table = [["  foo1", " bar2   ", "foo3"]]
+
+    # Table is not wrapped on 3 letters due to long word
+    expected = "h1    h2    h3\n----  ----  ----\nfoo1  bar2  foo3"
+    result = tabulate(test_table, table_headers, maxcolwidths=3, break_long_words=False)
+    assert_equal(expected, result)
+
+    # Table max width is 3 letters
+    expected = "h1    h2    h3\n----  ----  ----\nf     ba    foo\noo1   r2    3"
+    result = tabulate(test_table, table_headers, maxcolwidths=3, break_long_words=True)
+    assert_equal(expected, result)
+
+def test_break_on_hyphens():
+    "Output: Default table output, with break on hyphens true."
+    table_headers = ["h1", "h2", "h3"]
+    test_table = [["  foo-bar", " bar-bar   ", "foo-foo"]]
+    # Table max width is 5, long lines breaks on hyphens
+    expected = "h1    h2    h3\n----  ----  -----\nfoo   bar-  foo-f\n-bar  bar   oo"
+    result = tabulate(test_table, table_headers, maxcolwidths=5, break_on_hyphens=False)
+    assert_equal(expected, result)
+
+    # Table data is no longer breaks on hyphens
+    expected = "h1    h2    h3\n----  ----  ----\nfoo-  bar-  foo-\nbar   bar   foo"
+    result = tabulate(test_table, table_headers, maxcolwidths=5, break_on_hyphens=True)
+    assert_equal(expected, result)
